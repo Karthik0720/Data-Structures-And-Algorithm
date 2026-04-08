eb9cc24131db71b40712e9ba5af7a8cc730d8c1b
karthibri2007@gmail.com <karthibri2007@gmail.com>
2026-04-08 19:49:03 +0530

    Merge sorted array

---
+++ Arrays/Merge_Sorted_Array.py
@@ -0,0 +1,21 @@
+# Two pointer Approach
+
+# nums1 = [1,2,3,0,0,0] m=3
+# nums2 = [2,5,6] n=3   o/p = [1,2,2,3,5,6]
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+i=m-1
+j=n-1
+k=m+n-1
+while j>=0:
+    if i<0 or nums1[i]<=nums2[j]:
+        nums1[k]=nums2[j]
+        j-=1
+    else:
+        nums1[k]=nums1[i]
+        i-=1
+    k-=1
+print(nums1)
